698be357224afded7db5f94dcf1d7a56f5a2cba0
rodrigorocha1 <silva.rodrigo31@outlook.com>
2024-04-22 23:18:22 -0300

    mudando tipagem

--- service/youtube_assunto.py
+++ service/youtube_assunto.py
@@ -1,10 +1,10 @@
 from service.youtube_base import YoutubeBase
-from typing import List
+from typing import Dict
 
 
 class YoutubeAssunto(YoutubeBase):
-    def __init__(self, assuntos: List[str], data_inicio: str) -> None:
-        self._assuntos = assuntos
+    def __init__(self, assunto: str, data_inicio: str) -> None:
+        self._assunto = assunto
         self._data_inicio = data_inicio
         super().__init__()
 
@@ -12,16 +12,14 @@ class YoutubeAssunto(YoutubeBase):
         return self._url + '/search'
 
     @property
-    def params(self) -> List[str]:
-        return [
-            {
-                'part':  'snippet',
-                'key': self._KEY,
-                'regionCode': 'BR',
-                'relevanceLanguage': 'pt',
-                'maxResults': '50',
-                'publishedAfter': self._data_inicio,
-                'q': assunto,
-                'pageToken': ''
-            } for assunto in self._assuntos
-        ]
+    def params(self) -> Dict[str, str]:
+        return {
+            'part':  'snippet',
+            'key': self._KEY,
+            'regionCode': 'BR',
+            'relevanceLanguage': 'pt',
+            'maxResults': '50',
+            'publishedAfter': self._data_inicio,
+            'q': self._assunto,
+            'pageToken': ''
+        }
