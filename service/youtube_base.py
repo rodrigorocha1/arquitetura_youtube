e5896702a7f2b534963b0005a693e09f39f688f8
rodrigorocha1 <silva.rodrigo31@outlook.com>
2024-04-22 23:35:29 -0300

    correção param

--- service/youtube_base.py
+++ service/youtube_base.py
@@ -20,23 +20,20 @@ class YoutubeBase(ABC):
         req = requests.get(url=self.criar_url, params=params)
         return req.json()
 
-    def _executar_paginacao(self,  params: List[Dict]) -> Iterable[Dict]:
+    def _executar_paginacao(self,  param: Dict) -> Iterable[Dict]:
 
         i = 1
         next_token = ''
-        for param in params:
-            while next_token is not None:
-                response = self.__conectar_api(param)
-                if response:
-                    json_response = response.json()
-                    json_response['data_extracao'] = datetime.now().strftime(
-                        '%Y-%m-%d %H:%M:%S'
-                    )
-                    yield json_response
-                    try:
-                        next_token = json_response['nextPageToken']
-                        param['pageToken'] = next_token
-                    except KeyError:
-                        break
-                else:
-                    break
+
+        while next_token is not None:
+            response = self.__conectar_api(param)
+            if response:
+                json_response = response.json()
+                json_response['data_extracao'] = datetime.now().strftime(
+                    '%Y-%m-%d %H:%M:%S'
+                )
+                yield json_response
+                try:
+                    next_token = json_response['nextPageToken']
+                    param['pageToken'] = next_token
+                except KeyError:
