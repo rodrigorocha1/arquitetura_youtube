52fd39220d260e33b62f3f1351903977f4b0ff44
rodrigorocha1 <silva.rodrigo31@outlook.com>
2024-05-05 17:15:55 -0300

    classe Vídeo

--- service/youtube_video.py
+++ service/youtube_video.py
@@ -1,17 +1,14 @@
-from typing import Dict
 from service.youtube_base import YoutubeBase
 
 
 class YoutubeVideo(YoutubeBase):
-    def __init__(self, params: Dict) -> None:
+    def __init__(self, id_video: str) -> None:
         self._path_url = 'videos'
         self._params = {
-            'part': 'snippet',
-            'regionCode': 'BR',
+            'part': 'statistics,contentDetails,id,snippet,status',
+            'id': id_video,
             'relevanceLanguage': 'pt',
-            'maxResults': '50',
-            'q': assunto,
-            'publishedAfter': data_hora_busca,
+            'regionCode': 'BR'
 
         }
-        super().__init__(params, )
+        super().__init__(params=self._params, path_url=self._path_url)
