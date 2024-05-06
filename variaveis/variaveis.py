56b8aaeb7de911abf0f1a7cc7a2a7b595eb25dad
rodrigorocha1 <silva.rodrigo31@outlook.com>
2024-05-06 20:37:08 -0300

    Variavel do apache airflow

--- variaveis/variaveis.py
+++ variaveis/variaveis.py
@@ -1,4 +1,10 @@
-import os
+# import os
+from airflow.models import Variable
 
-chave = os.environ['chave_um']
-url = os.environ['url_youtube']
+
+# chave = os.environ['chave_um']
+# url = os.environ['url_youtube']
+
+
+chave = Variable.get('chave_um')
+url = Variable.get('url_youtube')
