c59eacb8bdaa042abc8ce60ca8d1a8b4b93bcbe4
rodrigorocha1 <silva.rodrigo31@outlook.com>
2024-05-06 21:01:12 -0300

    comentários e resposta comentários

--- dags/dag_hello_word.py
+++ dags/dag_hello_word.py
@@ -10,13 +10,15 @@ import pendulum
 from service.youtube_assunto import YoutubeAssunto
 from service.youtube_canal import YoutubeCanal
 from service.youtube_video import YoutubeVideo
+from service.youtube_comentario import YoutubeComentario
+from service.youtube_resposta_comentarios import YoutubeRespostaComentario
 from dados.infra_json import InfraJson
 from dados.infra_pickle import InfraPicke
 
 data_hora_atual = pendulum.now('America/Sao_Paulo').to_iso8601_string()
 data_hora_atual = pendulum.parse(data_hora_atual)
 data_hora_busca_path = data_hora_atual.strftime('%Y-%m-%dT%H:%M:%SZ')
-data_hora_busca = data_hora_atual.subtract(hours=10)
+data_hora_busca = data_hora_atual.subtract(hours=6)
 data_hora_busca = data_hora_busca.strftime('%Y-%m-%dT%H:%M:%SZ')
 path_data = data_hora_busca_path.replace(
     "-", "_").replace("T", "_").replace("Z", "").replace(":", "_")
@@ -113,6 +115,71 @@ def task_obter_dados_videos(assunto: str, path_data: str):
         ij.salvar_dados(req)
 
 
+@task
+def task_obter_dados_comentarios(assunto: str, path_data: str):
+    ifp = InfraPicke(
+        diretorio_datalake='bronze',
+        termo_assunto=assunto,
+        metrica=None,
+        path_data=None,
+        nome_arquivo='lista_id_videos_comentarios.pkl'
+    )
+
+    lista_videos_comentarios = ifp.carregar_dados()
+
+    for video_comentario in lista_videos_comentarios:
+        yc = YoutubeComentario(id_video=video_comentario)
+        pgs = yc.executar_paginacao()
+        for pg in pgs:
+
+            lista_resposta_comentarios = yc.obter_resposta_comentarios(req=pg)
+            ij = InfraJson(
+                diretorio_datalake='bronze',
+                termo_assunto=assunto,
+                metrica='comentarios',
+                path_data=f'extracao_{path_data}',
+                nome_arquivo='req.json'
+            )
+            ij.salvar_dados(req=pg)
+
+            ifp = InfraPicke(
+                diretorio_datalake='bronze',
+                termo_assunto=assunto,
+                metrica=None,
+                path_data=None,
+                nome_arquivo='lista_resposta_comentarios.pkl'
+            )
+            ifp.salvar_dados(lista=lista_resposta_comentarios)
+
+
+@task
+def task_ober_resposta_comentarios(assunto: str, path_data: str):
+    ifp = InfraPicke(
+        diretorio_datalake='bronze',
+        termo_assunto=assunto,
+        metrica=None,
+        path_data=None,
+        nome_arquivo='lista_resposta_comentarios.pkl'
+    )
+
+    lista_videos_resposta_comentarios = ifp.carregar_dados()
+    for video_resposta_comentarios in lista_videos_resposta_comentarios:
+        if video_resposta_comentarios[1] > 0:
+            yrc = YoutubeRespostaComentario(
+                id_resposta_comentario=video_resposta_comentarios[0]
+            )
+            pgs = yrc.executar_paginacao()
+            for pg in pgs:
+                ij = InfraJson(
+                    diretorio_datalake='bronze',
+                    termo_assunto=assunto,
+                    metrica='respota_comentarios',
+                    path_data=f'extracao_{path_data}',
+                    nome_arquivo='req.json'
+                )
+                ij.salvar_dados(req=pg)
+
+
 @dag(
     start_date=datetime(2023, 8, 1),
     schedule=None,
@@ -152,8 +219,22 @@ def dag_youtube():
                 task_id=f"assunto_{assunto.replace(' ', '_').lower()}"
             )(assunto, path_data)
 
+    @task_group
+    def obter_dados_comentarios():
+        for assunto in lista_assunto:
+            task_obter_dados_comentarios.override(
+                task_id=f"assunto_{assunto.replace(' ', '_').lower()}"
+            )(assunto, path_data)
+
+    @task_group
+    def obter_dados_resposta_comentarios():
+        for assunto in lista_assunto:
+            task_ober_resposta_comentarios.override(
+                task_id=f"assunto_{assunto.replace(' ', '_').lower()}"
+            )(assunto, path_data)
+
     inicio_dag() >> obter_assunto() >> obter_dados_canais(
-    ) >> obter_dados_videos() >> fim_dag()
+    ) >> obter_dados_videos() >> obter_dados_comentarios() >> obter_dados_resposta_comentarios() >> fim_dag()
 
 
 dag_youtube()
